50df3836734c58c000333f918ed0ec7a1c09ac27
Chidambaresh <124269988+icy-chidam@users.noreply.github.com>
2026-05-04 21:00:38 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -12,28 +12,36 @@ module tb();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Dump waves
+`ifdef GL_TEST
+  // For gate-level simulation - power and ground are required
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
   end
 
-  // Instantiate the design
+  // Instantiate the design – make sure the module name matches your top-level module
   tt_um_chidam_secengine uut (
-    .ui_in(ui_in),
-    .uo_out(uo_out),
-    .uio_in(uio_in),
+`ifdef GL_TEST
+    .VPWR(VPWR),
+    .VGND(VGND),
+`endif
+    .ui_in  (ui_in),
+    .uo_out (uo_out),
+    .uio_in (uio_in),
     .uio_out(uio_out),
-    .uio_oe(uio_oe),
-    .ena(ena),
-    .clk(clk),
-    .rst_n(rst_n)
+    .uio_oe (uio_oe),
+    .ena    (ena),
+    .clk    (clk),
+    .rst_n  (rst_n)
   );
 
-  // Clock generation
+  // Clock generator (50 MHz)
   always #10 clk = ~clk;
 
-  // Initial values
   initial begin
     clk = 0;
     rst_n = 0;
@@ -44,7 +52,7 @@ module tb();
     rst_n = 1;
     #20;
     ena = 1;
-    // The test will be driven by cocotb, so we just wait forever
+    // Let cocotb test drive the inputs from now on
     #1000000;
     $finish;
   end
